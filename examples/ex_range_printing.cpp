b71474b62aed7ed4425042c69b3957fe50f03222
Thomas HOULLIER <pro@houllier.net>
2023-10-21 09:56:31 +0200

    Added an assert against printing an infinite sequence.

--- examples/ex_range_printing.cpp
+++ examples/ex_range_printing.cpp
@@ -2,19 +2,25 @@
 #include <ranges>
 #include <fmt/ranges.h>
 
+class RangePrinter {
+public:
+  template<std::ranges::view V>
+  void print (const V& range) {
+    static_assert(std::ranges::sized_range<V>,
+                  "Range must be a sized_range for printing!");
+    fmt::print("{}\n", range);
+  }
+};
+
 template<std::ranges::view V>
-void print_range (const V& range) {
-  fmt::print("{}\n", range);
+const V& operator| (const V& range, RangePrinter rp) {
+  rp.print(range);
+  return range;
 }
 
-class RangePrinter {};
-
-template<std::ranges::view V>
-void operator| (const V& range, RangePrinter rp) {
-  print_range(range);
-}
+RangePrinter rangePrinter;
 
 int main () {
   auto range_toprint = std::views::iota(1,6);
-  range_toprint | RangePrinter();
+  range_toprint | rangePrinter | std::views::take(3) | rangePrinter;
 }
