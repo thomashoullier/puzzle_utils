1eebe3002c495c80bf32903ff8baa3408ac17afa
Thomas HOULLIER <pro@houllier.net>
2023-10-21 15:11:17 +0200

    First full implementation of the range printer using std::enable_if_t.

--- examples/ex_range_printing.cpp
+++ examples/ex_range_printing.cpp
@@ -4,12 +4,28 @@
 
 class RangePrinter {
 public:
-  template<std::ranges::view V>
-  void print (const V& range) {
-    static_assert(std::ranges::sized_range<V>,
-                  "Range must be a sized_range for printing!");
+  RangePrinter () {
+    print_length = 10;
+  };
+
+  RangePrinter (std::size_t _print_length) {
+    print_length = _print_length;
+  };
+
+  template <typename R,
+            std::enable_if_t<std::ranges::sized_range<R>, bool> = true>
+  void print (const R& range) {
     fmt::print("{}\n", range);
   }
+
+  template <typename R,
+            std::enable_if_t<!std::ranges::sized_range<R>, bool> = true>
+  void print (const R& range) {
+    fmt::print("{}\n", range | std::views::take(print_length));
+  }
+
+private:
+  std::size_t print_length;
 };
 
 template<std::ranges::view V>
@@ -23,4 +39,8 @@ RangePrinter rangePrinter;
 int main () {
   auto range_toprint = std::views::iota(1,6);
   range_toprint | rangePrinter | std::views::take(3) | rangePrinter;
+  range_toprint | rangePrinter;
+  //range_toprint | RangePrinter(3) | rangePrinter;
+  std::views::iota(1) | RangePrinter(3);
+  std::views::iota(1) | rangePrinter;
 }
