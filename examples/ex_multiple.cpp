8c5a61f78eba47b20497e21de70ca4a660163c18
Thomas HOULLIER <pro@houllier.net>
2023-10-22 19:55:57 +0200

    Added an example for implementing a is_multiple_of function.

---
+++ examples/ex_multiple.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <ranges>
+
+template <typename... Args>
+auto is_multiple_of (Args... args) {
+  return [args...](int x) {
+    return (... && (x % args == 0));
+  };
+}
+
+int main () {
+  auto r = std::views::iota(1, 20) | std::views::filter(is_multiple_of(2, 3));
+
+  for (int i : r) {
+    std::cout << i << " ";
+  } std::cout << std::endl;
+}
