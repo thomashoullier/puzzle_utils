42f672ad94ac3d907c08537a09425a7094e5dd50
Thomas HOULLIER <pro@houllier.net>
2023-10-29 19:05:20 +0100

    Illustrated standard ways of summing the elements of a range.

---
+++ examples/ex_range_sum.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <ranges>
+#include <algorithm>
+#include <numeric>
+#include <pzu.hpp>
+
+int main () {
+  auto r = std::views::iota(1,20);
+  r | pzu::rangePrinter;
+
+  // Sum using fold_left
+  auto fold_sum = std::ranges::fold_left(r, 0, std::plus<int>());
+  std::cout << "fold_left: " << fold_sum << std::endl;
+
+  // Sum using accumulate
+  auto acc_sum = std::accumulate(r.begin(), r.end(), 0);
+  std::cout << "accumulate: " << acc_sum << std::endl;
+
+  // reduce
+  auto red_sum = std::reduce(r.begin(), r.end());
+  std::cout << "reduce: " << red_sum << std::endl;
+}
