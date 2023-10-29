7ff3ba0692dfcdcd74cbba8b26fa571ea4bdd1af
Thomas HOULLIER <pro@houllier.net>
2023-10-29 07:38:46 +0100

    Drafted an update to is_multiple_of.

--- examples/ex_multiple.cpp
+++ examples/ex_multiple.cpp
@@ -1,11 +1,35 @@
 #include <iostream>
 #include <pzu.hpp>
 
-int main () {
-  auto r = std::views::iota(1, 20)
-    | std::views::filter(pzu::is_multiple_of(2, 3));
+template <typename Arg>
+auto is_multiple_of (Arg arg) {
+  return [arg](const auto x) {
+    return (x % arg == 0);
+  };
+}
 
-  for (int i : r) {
-    std::cout << i << " ";
-  } std::cout << std::endl;
+template <typename... Args>
+auto is_multiple_of_any (Args... args) {
+  return [args...](const auto x) {
+    return (... or (x % args == 0));
+  };
+}
+
+template <typename... Args>
+auto is_multiple_of_all (Args... args) {
+  return [args...](const auto x) {
+    return (... and (x % args == 0));
+  };
+}
+
+int main () {
+  std::views::iota(1,20)
+    | std::views::filter(is_multiple_of(3))
+    | pzu::rangePrinter;
+  std::views::iota(1,20)
+    | std::views::filter(is_multiple_of_any(3,5))
+    | pzu::rangePrinter;
+  std::views::iota(1,20)
+    | std::views::filter(is_multiple_of_all(3,5))
+    | pzu::rangePrinter;
 }
